3b85a0fef06b3b0ba7dec91774bdac77495b8669
diasEduardo <eduardo_dududex@hotmail.com>
2016-03-10 18:17:39 -0300

    Revert "Revert "add topicos a lista""
    This reverts commit cc75bab426e50004cd6d12fae53633017abb7a67.

--- app/src/main/java/br/com/timeba/eduardo/desafiomoblee/topicos.java
+++ app/src/main/java/br/com/timeba/eduardo/desafiomoblee/topicos.java
@@ -2,6 +2,11 @@ package br.com.timeba.eduardo.desafiomoblee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class topicos extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class topicos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topicos);
+        ListView topicos = (ListView) findViewById(R.id.list_view_topicos);
+        String[] itens={"Android","iOS","Android Studio","Xcode","Swift"};
+        ArrayList<String> arrayList= new ArrayList<String>(Arrays.asList(itens));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.listItem,arrayList);
+        topicos.setAdapter(adapter);
+
     }
 }
