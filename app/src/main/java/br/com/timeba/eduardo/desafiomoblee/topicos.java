3c9038ce86c1bededce4b99433d37ffb9f54caae
diasEduardo <eduardo_dududex@hotmail.com>
2016-03-13 18:38:54 -0300

    add onClick
    adicionado o metodo onclick para a lista de topicos.

--- app/src/main/java/br/com/timeba/eduardo/desafiomoblee/topicos.java
+++ app/src/main/java/br/com/timeba/eduardo/desafiomoblee/topicos.java
@@ -1,14 +1,17 @@
 package br.com.timeba.eduardo.desafiomoblee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class topicos extends AppCompatActivity {
+public class Topicos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,18 @@ public class topicos extends AppCompatActivity {
         ListView topicos = (ListView) findViewById(R.id.list_view_topicos);
         String[] itens={"Android","iOS","Android Studio","Xcode","Swift"};
         ArrayList<String> arrayList= new ArrayList<String>(Arrays.asList(itens));
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.listItem,arrayList);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.listItem,arrayList);
         topicos.setAdapter(adapter);
-        topicos
+
+        topicos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String nome = adapter.getItem(position).toLowerCase().replace(" ","-");
+                Intent i =new Intent(Topicos.this,Questoes.class);
+                i.putExtra("nome",nome);
+                startActivity(i);
+            }
+        });
     }
 }
